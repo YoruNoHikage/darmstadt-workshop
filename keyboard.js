2567740164f36aae2ec4e6441f7b665284d63576
Alexis Launay <yorunohikage@gmail.com>
2018-04-14 14:36:37 +0200

    Update keyboard.js

--- keyboard.js
+++ keyboard.js
@@ -1,12 +1,12 @@
-document.onkeypress = function(e) {
+document.addEventListener('keydown', function(e) {
   e = e || window.event;
   var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
 
-  if (window.updateFontsWithKeyboard) {
-    switch (charCode) {
-      case 32:
-        window.updateFontsWithKeyboard('space');
-        break;
-    }
+  window.updateFontsWithKeyboard(event.key);
+});
+
+document.body.addEventListener('mousepress', function () {
+  if (window.updateFontsWithClick) {
+    window.updateFontsWithClick();
   }
-};
+})
